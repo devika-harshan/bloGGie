e1220c86945d9548a8d2e63a3824ba2194048ab9
Devika Harshan <85967317+devika-harshan@users.noreply.github.com>
2023-01-11 10:06:42 +0530

    Update Profile.dart

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -1,4 +1,5 @@
 import 'package:bloggie/editprofile.dart';
+import 'package:bloggie/welcome.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -28,75 +29,13 @@ class _ProfileState extends State<Profile> {
     final String abt = Provider.of<UserState>(context).userAbout;
     return Scaffold(
       body: // Generated code for this Stack Widget...
-          Stack(
-        children: [
           Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                width: 4,
-                height: 4,
-                clipBehavior: Clip.antiAlias,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                ),
-                child: Image.network(
-                  'https://picsum.photos/seed/737/600',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
+        children: [
           Container(
-            width: 415.6,
-            height: 271.3,
-            decoration: BoxDecoration(
-              color: Color(0xffad6bb9),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(20),
-                bottomRight: Radius.circular(20),
-              ),
-            ),
-            child: Stack(
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Align(
-                  alignment: AlignmentDirectional(0.02, -0.32),
-                  child: Container(
-                    width: 120,
-                    height: 120,
-                    clipBehavior: Clip.antiAlias,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                    ),
-                    child: usersex == gen
-                        ? Image.network(
-                            'https://static.vecteezy.com/system/resources/previews/004/819/327/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg',
-                            fit: BoxFit.cover,
-                          )
-                        : Image.network(
-                            'https://static.vecteezy.com/system/resources/previews/004/819/322/original/female-avatar-woman-profile-icon-for-network-vector.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                  ),
-                ),
-                Align(
-                  alignment: AlignmentDirectional(0, 0.51),
-                  child: Text(
-                    '@' + name,
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: Colors.white,
-                        fontWeight: FontWeight.w400),
-                  ),
-                ),
-                Align(
-                  alignment: AlignmentDirectional(-0.02, 0.80),
-                  child: Text(fname,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20, color: Colors.white)),
-                ),
                 Align(
                   alignment: AlignmentDirectional(0.84, -0.82),
                   child: InkWell(
@@ -113,62 +52,107 @@ class _ProfileState extends State<Profile> {
                     ),
                   ),
                 ),
+                Container(
+                  width: 120,
+                  height: 120,
+                  clipBehavior: Clip.antiAlias,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                  ),
+                  child: usersex == gen
+                      ? Image.network(
+                          'https://static.vecteezy.com/system/resources/previews/004/819/327/original/male-avatar-profile-icon-of-smiling-caucasian-man-vector.jpg',
+                          fit: BoxFit.cover,
+                        )
+                      : Image.network(
+                          'https://static.vecteezy.com/system/resources/previews/004/819/322/original/female-avatar-woman-profile-icon-for-network-vector.jpg',
+                          fit: BoxFit.cover,
+                        ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    '@' + name,
+                    style: TextStyle(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400),
+                  ),
+                )
               ],
             ),
+            height: 270,
+            decoration: BoxDecoration(
+              color: Color(0xff744481),
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15.0),
-            child: Align(
-              alignment: AlignmentDirectional(-0.90, 0.07),
-              child: Text(abt,
-                  textAlign: TextAlign.center,
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 25, left: 18.0, bottom: 10),
+                child: Text(
+                  fname,
                   style: TextStyle(
                       fontWeight: FontWeight.w700,
                       fontSize: 17,
-                      color: Color(0xff575656))),
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(-0.90, 0.19),
-            child: Divider(
+                      color: Color(0xff575656)),
+                ),
+              ),
+              Divider(
+                thickness: 1.5,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, left: 18.0, bottom: 15),
+                child: Text(
+                  abt,
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 17,
+                      color: Color(0xff575656)),
+                ),
+              ),
+              Divider(
               thickness: 1.5,
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(0, 0.29),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Row(
-                children: <Widget>[
-                  Icon(Icons.location_on),
-                  Text(loctn,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 20, left: 10.0, bottom: 10),
+                child: Row(
+                  children: <Widget>[
+                    Icon(Icons.location_on),
+                    Text(
+                      loctn,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w700,
+                          fontSize: 17,
+                          color: Color(0xff575656)),
+                    )
+                  ],
+                ),
+              ),
+              Divider(
+                thickness: 1.5,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 15, left: 18.0, bottom: 15),
+                child: InkWell(
+                  onTap: () async {
+                    await FirebaseAuth.instance.signOut();
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => welcome()));
+                  },
+                  child: Text('Logout',
                       style: TextStyle(
                           fontWeight: FontWeight.w700,
                           fontSize: 17,
                           color: Color(0xff575656))),
-                ],
+                ),
               ),
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(-0.90, 0.39),
-            child: Divider(
-              thickness: 1.5,
-            ),
-          ),
-          Align(
-            alignment: AlignmentDirectional(-0.90, 0.46),
-            child: InkWell(
-              onTap: () async {
-                await FirebaseAuth.instance.signOut();
-              },
-              child: Text("Logout",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 17,
-                      color: Color(0xff575656))),
-            ),
+            ],
           ),
         ],
       ),
