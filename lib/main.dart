393ca2998e4898568972141e8fcf1f53367e7fd4
Devika Harshan <85967317+devika-harshan@users.noreply.github.com>
2023-01-13 09:31:07 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,6 @@
 import 'UserState.dart';
-
 import 'package:bloggie/welcome.dart';
-
 import 'package:flutter/material.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
 import 'firebase_options.dart';
